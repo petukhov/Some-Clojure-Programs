01790f8063f9a40b8716985d3446a65a3ae0ecbc
Georgy Petukhov <gpetukhov@me.com>
2016-05-11 03:55:19 -0500

    it all works with brackets too

--- src/cool_clojure_snippets/infix_arithmetic.clj
+++ src/cool_clojure_snippets/infix_arithmetic.clj
@@ -2,7 +2,9 @@
 
 (def allowed-ops {+ 1, - 1, * 2, / 2})
 
-(defn- process-operator [[ops-stack output] operator]
+(defn resolve-sym [sym] @(resolve (symbol sym)))
+
+(defn process-operator [[ops-stack output] operator]
   (let [operator-on-stack (peek ops-stack)]
     (if (or (empty? ops-stack) (< (allowed-ops operator-on-stack) (allowed-ops operator)))
       [(conj ops-stack operator) output]
@@ -10,25 +12,30 @@
           (assoc 1 (conj output operator-on-stack))         ; and putting that operator on the output stack
           (process-operator operator)))))
 
-(defn- process-token [[ops-stack output] token]
+(defn process-token [[ops-stack output] token]
   (cond
+    (list? token) [ops-stack (conj output (apply infix token))]
     (number? token) [ops-stack (conj output token)]
-    (contains? allowed-ops token) (process-operator [ops-stack output] token)
-    :else (throw (Exception. "Macro supplied with invalid data"))))
+    (and (symbol? token) (contains? allowed-ops (resolve-sym token))) (process-operator [ops-stack output] (resolve-sym token))
+    :else (throw (Exception. (str "Macro supplied with invalid data: " token)))))
 
-(defn- forms->postfix [expr]
+(defn forms->postfix [expr]
   (let [[ops-stack output] (reduce process-token [() ()] expr)]
     (into ops-stack output)))                               ; since ops-stack is not empty, we need to merge it with output
 
-(defn- calculate [stack token]
+(defn calculate [stack token]
   (if (number? token)
     (conj stack token)
     (conj (drop 2 stack) (token (second stack) (first stack)))))
 
-(defn- postfix->value [postfix-stack]
+(defn postfix->value [postfix-stack]
   (first (reduce calculate () postfix-stack)))
 
 (defn infix [& expr]
   (-> expr
       (forms->postfix)
       (postfix->value)))
+
+(defmacro infix-macro
+  [& elements]
+  `(apply infix '~elements))
